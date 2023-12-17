8c505918efdf09359c419c6a582113de2eb6aaa0
Aadarsh <aadarshkumar8871860855@gmail.com>
2023-12-17 13:52:54 +0530

    changes

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        DATABASE_URL: process.env.DATABASE_URL,
-        NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-        NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET
-    }
+    env: {}
 }
 
 module.exports = nextConfig
